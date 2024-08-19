bd40d5503afa9109d9c9cd320f3bd86a0d6b4bb8
Merdekasoft <merdekasoft@gmail.com>
2024-08-19 18:59:09 +0700

    Update cpu_usage_emoji.sh

--- cpu_usage_emoji.sh
+++ cpu_usage_emoji.sh
@@ -4,12 +4,18 @@
 cpu_usage=$(mpstat 1 1 | awk '/Average/ {printf "%d", 100 - $12}')
 
 # Choose the appropriate emoji based on CPU usage
-if [ "$cpu_usage" -lt 20 ]; then
-    emoji="😄"  # Low CPU usage, happy
-elif [ "$cpu_usage" -lt 70 ]; then
+if [ "$cpu_usage" -lt 10 ]; then
+    emoji="😄"  # Very low CPU usage, happy
+elif [ "$cpu_usage" -lt 30 ]; then
+    emoji="😊"  # Low CPU usage, smiling
+elif [ "$cpu_usage" -lt 50 ]; then
     emoji="🙂"  # Medium CPU usage, neutral
+elif [ "$cpu_usage" -lt 70 ]; then
+    emoji="😐"  # Slightly high CPU usage, meh
+elif [ "$cpu_usage" -lt 90 ]; then
+    emoji="😟"  # High CPU usage, worried
 else
-    emoji="😡"  # High CPU usage, angry
+    emoji="😡"  # Very high CPU usage, angry
 fi
 
 # Print the emoji followed by the CPU usage percentage
